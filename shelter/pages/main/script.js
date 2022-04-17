2be87677ccb2fd08bfc510d2d30b84829aea61c4
Mikhail Nosov <manosov1984@gmail.com>
2022-04-18 00:43:08 +0400

    feat: add modal-window

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -15,3 +15,48 @@ const closeMenu = () => {
 navigation.addEventListener('click', e => {
   if (e.target.classList.contains('navigation__link')) closeMenu()
 });
+
+
+/**Modal-window */
+
+const petsList = document.querySelector('.pets-list__list');
+const pageOverlayModal = document.querySelector('.page__overlay_modal');
+const modalClose = document.querySelector('.modal__close');
+const modal = document.querySelector('.modal');
+
+const showModal = (elem, param) => {
+  let opacity = 0;
+  elem.opacity = opacity;
+  elem.style.display = '';
+
+  const animation = () => {
+    opacity += param;
+    elem.style.opacity = opacity;
+    if (opacity < 1) requestAnimationFrame(animation);
+  }
+  requestAnimationFrame(animation)
+};
+
+const hideModal = (elem, param) => {
+  let opacity = getComputedStyle(elem).getPropertyValue('opacity');
+  const animation = () => {
+    opacity -= param;
+    elem.style.opacity = opacity;
+    if (opacity > 0) requestAnimationFrame(animation);
+    else elem.style.display = 'none';
+  }
+  requestAnimationFrame(animation);
+};
+
+
+petsList.addEventListener('click', e => {
+  if (e.target.classList.contains('pets-list__button')) {
+    pageOverlayModal.style.display = 'block';
+    showModal(modal, 0.03)
+  }
+});
+
+modalClose.addEventListener('click', () => {
+  hideModal(modal, 0.03)
+  pageOverlayModal.style.display = 'none';
+})
