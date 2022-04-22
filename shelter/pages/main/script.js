6908ab4c9a9d305d43267da87eb39c3884414a81
nos64 <manosov1984@gmail.com>
2022-04-22 10:23:29 +0400

    feat: add popup main page

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -152,156 +152,13 @@ petsListWrapper.addEventListener('click', e => {
   if (e.target.classList.contains('pets-list__pagination-button')) {
  
   }
-})
-
-
-
-/* <div class="page__overlay page__overlay_modal">
-<div class="modal">
-  <img class="modal__image" src="../../assets/images/pets-jennifer.png" alt="">
-  <div class="modal__text-wapper">
-    <h3 class="modal__text-name">Jennifer</h3>
-    <p class="modal__text-type">Dog - Labrador</p>
-    <p class="modal__text-story">
-      Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won't hesitate to play up a storm in the house if she has all of her favorite toys.
-    </p>
-    <ul class="modal__text-list">
-      <li class="modal__text-item"><span class="text-item__text"><b>Age: </b><span class="modal__text-param">2 months</span></span></li>
-      <li class="modal__text-item"><span class="text-item__text"><b>Inoculations: </b><span class="modal__text-param">none</span></span></li>
-      <li class="modal__text-item"><span class="text-item__text"><b>Diseases: </b><span class="modal__text-param">none</span></span></li>
-      <li class="modal__text-item"><span class="text-item__text"><b>Parasites: </b><span class="modal__text-param">none</span></span></li>
-    </ul>
-  </div>
-  <button class="modal__close">
-    <svg width="12" height="12" viewbox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M7.42618 6.00003L11.7046 1.72158C12.0985 1.32775 12.0985 0.689213 11.7046 0.295433C11.3108 -0.0984027 10.6723 -0.0984027 10.2785 0.295433L5.99998 4.57394L1.72148 0.295377C1.32765 -0.098459 0.68917 -0.098459 0.295334 0.295377C-0.0984448 0.689213 -0.0984448 1.32775 0.295334 1.72153L4.57383 5.99997L0.295334 10.2785C-0.0984448 10.6723 -0.0984448 11.3108 0.295334 11.7046C0.68917 12.0985 1.32765 12.0985 1.72148 11.7046L5.99998 7.42612L10.2785 11.7046C10.6723 12.0985 11.3108 12.0985 11.7046 11.7046C12.0985 11.3108 12.0985 10.6723 11.7046 10.2785L7.42618 6.00003Z" fill="#292929"/>
-      </svg>
-  </button>
-</div>
-</div> */
-
-const renderModal = (data) => {
-  let modals = [];
-  const pageOverlayModal = document.createElement('div');
-  pageOverlayModal.classList.add('page__overlay')
-  pageOverlayModal.classList.add('page__overlay_modal')
-
-  for (let i = 0; i < data.length; i++) {
-    pageOverlayModal.dataset.name = data[i].name
-    pageOverlayModal.innerHTML = `
-    <div class="modal">
-    <div class="modal__container">
-      <img class="modal__image" src="${data[i].img}" alt="">
-      <div class="modal__text-wapper">
-        <h3 class="modal__text-name">${data[i].name}</h3>
-        <p class="modal__text-type">${data[i].type} - ${data[i].breed}</p>
-        <p class="modal__text-story">
-        ${data[i].description}
-        </p>
-        <ul class="modal__text-list">
-        <li class="modal__text-item"><span class="text-item__text"><b>Age: </b><span class="modal__text-param age">${data[i].age}</span></span></li>
-        <li class="modal__text-item"><span class="text-item__text"><b>Inoculations: </b><span class="modal__text-param inoculations">${data[i].inoculations}</span></span></li>
-        <li class="modal__text-item"><span class="text-item__text"><b>Diseases: </b><span class="modal__text-param diseases">${data[i].diseases}</span></span></li>
-        <li class="modal__text-item"><span class="text-item__text"><b>Parasites: </b><span class="modal__text-param parasites">${data[i].parasites}</span></span></li>
-        </ul>
-      </div>
-      <button class="modal__close">
-        <svg width="12" height="12" viewbox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd" clip-rule="evenodd" d="M7.42618 6.00003L11.7046 1.72158C12.0985 1.32775 12.0985 0.689213 11.7046 0.295433C11.3108 -0.0984027 10.6723 -0.0984027 10.2785 0.295433L5.99998 4.57394L1.72148 0.295377C1.32765 -0.098459 0.68917 -0.098459 0.295334 0.295377C-0.0984448 0.689213 -0.0984448 1.32775 0.295334 1.72153L4.57383 5.99997L0.295334 10.2785C-0.0984448 10.6723 -0.0984448 11.3108 0.295334 11.7046C0.68917 12.0985 1.32765 12.0985 1.72148 11.7046L5.99998 7.42612L10.2785 11.7046C10.6723 12.0985 11.3108 12.0985 11.7046 11.7046C12.0985 11.3108 12.0985 10.6723 11.7046 10.2785L7.42618 6.00003Z" fill="#292929"/>
-          </svg>
-      </button>
-    </div>
-    </div>
-    `;
-
-    document.body.append(pageOverlayModal);
-    modals.push(pageOverlayModal);
-  }
-  // console.log(modals)
-
-  return modals;
-}
-
-
-// const renderModal = (data) => {
-//   const modalTextName = document.querySelector('.modal__text-name');
-//   const modalImage = document.querySelector('.modal__image');
-//   const modalType = document.querySelector('.type');
-//   const modalBreed = document.querySelector('.breed');
-//   const modalDescription = document.querySelector('.description');
-//   const modalAge = document.querySelector('.age');
-//   const modalInoculations = document.querySelector('.inoculations');
-//   const modalDiseases = document.querySelector('.diseases');
-//   for (let i = 0; i < data.length; i++) {
-//     modalTextName.textContent = data[i].name;
-//     modalImage.src = data[i].img;
-//     modalType.textConten = data[i].type;
-//     modalBreed.textContent = data[i].breed;
-//     modalDescription.textContent = data[i].description;
-//     modalAge.textContent = data[i].age;
-//     modalInoculations.textContent = data[i].inoculations;
-    
-//   }
-// return data;
-// }
-
-
-const modalClick = (modals) => {
-  console.log(modals)
-  let dataSet ='';
-  // modals.forEach(modal => {
-    // console.log(modal)
-    petsList.addEventListener('click', e => {
-      if (e.target.closest('.pets-list__item')) {
-        dataSet = e.target.dataset.name;
-        console.log('dataSet: ', dataSet);
-      }
-        const popup = modals.find(item => {
-
-          
-          item.name === e.target.dataset.name
-        } )
-        console.log(popup)
-        // showModal(popup)
-
-        // pageOverlayModal.classList.add('page__overlay_modal_open');
-        // showElem(modal, 0.03);
-      
-    // });
-  })  
-  // if (event.target.closest('.pets-list__item')) {
-  //   let dataSet = e.target.dataset.name;
-    
-
-  //   pageOverlayModal.classList.add('page__overlay_modal_open');
-  //   showElem(modal, 0.03);
-  // }
-}
-
-
-
-
-
-
-
-/**Start page */
-  const initPage = async () => {
-    const data = await getData();
-    const cards = renderCard(data);
-    const modals = renderModal(data);
-
-    
-    showCard(cards);
-    modalClick(modals);
-  }
-
-  initPage();
+});
 
 
 /**Modal-window */
 const pageOverlayModal = document.querySelector(".page__overlay_modal");
 const modalClose = document.querySelector('.modal__close');
-const modal = document.querySelector('.modal');
+// const modal = document.querySelector('.modal');
 
 const openModal = () => {
   disableScroll();
@@ -315,20 +172,65 @@ const closeModal = () => {
   enableScroll();
 };
 
-petsList.addEventListener('click', e => {
-if (e.target.closest('.pets-list__item')) {
-  let dataSet = e.target.dataset.name;
-  openModal();
-  pageOverlayModal.classList.add('page__overlay_modal_open');
+const renderModal = modal => {
+  const modalTextName = document.querySelector('.modal__text-name');
+  const modalImage = document.querySelector('.modal__image');
+  const modalType = document.querySelector('.type');
+  const modalBreed = document.querySelector('.breed');
+  const modalDescription = document.querySelector('.description');
+  const modalAge = document.querySelector('.age');
+  const modalInoculations = document.querySelector('.inoculations');
+  const modalDiseases = document.querySelector('.diseases');
+  const modalParasites = document.querySelector('.parasites');
+  
+  modalTextName.textContent = modal.name;
+  modalImage.src = modal.img;
+  modalType.textConten = modal.type;
+  modalBreed.textContent = modal.breed;
+  modalDescription.textContent = modal.description;
+  modalAge.textContent = modal.age;
+  modalInoculations.textContent = modal.inoculations;
+  modalDiseases.textContent = modal.diseases;
+  modalParasites.textContent = modal.parasites;
+};
+
+const getModal = data => {
+  petsList.addEventListener('click', e => {
+    if (e.target.closest('.pets-list__item')) {
+      const modal = data.find(item => item.name === e.target.dataset.name);
+      renderModal(modal);
+      openModal();
+    }
+  });
 }
-});
 
 /**Modal-window close */
-// modalClose.addEventListener('click', () => {
-// enableScroll();
-// closeModal();
-// });
+modalClose.addEventListener('click', () => {
+enableScroll();
+closeModal();
+});
 
-// pageOverlayModal.addEventListener('click', closeModal);
+pageOverlayModal.addEventListener('click', closeModal);
+
+
+
+
+
+
+/**Start page */
+  const initPage = async () => {
+    const data = await getData();
+    const cards = renderCard(data);
+
+    getModal(data);
+    showCard(cards);
+
+  }
+
+  initPage();
+
+
+
+
 
 
