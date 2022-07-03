d40790635ac59d57b696d3e65333436a6ed60b09
Mikhail Nosov <manosov1984@gmail.com>
2022-07-04 00:47:26 +0400

    fix: change API-link

--- migration-to-TypeScript/src/components/controller/appLoader.ts
+++ migration-to-TypeScript/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://newsapi.org/v2/', {
+    super('https://nodenews.herokuapp.com/', {
       // apiKey: '6831fdc5786042f5aed5874e2cea018c',
       // apiKey: 'fe098bb13b224e6bbeaedb4d76792476',
       apiKey: '98b26295ad584728b3240f2349d8da08',
