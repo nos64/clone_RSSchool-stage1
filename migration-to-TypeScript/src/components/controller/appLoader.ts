570b395df045ace428fe62fbb0d1eec32a23c7f3
Mikhail Nosov <manosov1984@gmail.com>
2022-07-05 01:45:28 +0400

    feat: hide API-KEY

--- migration-to-TypeScript/src/components/controller/appLoader.ts
+++ migration-to-TypeScript/src/components/controller/appLoader.ts
@@ -1,11 +1,9 @@
 import Loader from './loader';
-
+import { API_KEY } from '../../../KEY';
 class AppLoader extends Loader {
   constructor() {
     super('https://nodenews.herokuapp.com/', {
-      // apiKey: '6831fdc5786042f5aed5874e2cea018c',
-      // apiKey: 'fe098bb13b224e6bbeaedb4d76792476',
-      apiKey: '98b26295ad584728b3240f2349d8da08',
+      apiKey: API_KEY,
     });
   }
 }
