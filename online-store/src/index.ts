6d3b5d0f3daa8b48467a6239a2d62174f175b057
Mikhail Nosov <manosov1984@gmail.com>
2022-07-06 22:59:54 +0400

    feat: add db, image & fonts

---
+++ online-store/src/index.ts
@@ -0,0 +1,8 @@
+import './index.html';
+import './index.scss';
+
+import { multi, sum } from './modules/calc';
+
+
+console.log(multi(5, 2));
+console.log(sum(5, 10));
---
+++ online-store/src/modules/calc.ts
@@ -0,0 +1,2 @@
+export const multi = (a: number, b: number): number => a * b;
+export const sum =  (a: number, b: number): number => a + b;
