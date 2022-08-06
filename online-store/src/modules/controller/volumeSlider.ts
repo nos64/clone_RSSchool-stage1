5f90265bbe1c9500c11bb940bd34997343407fad
nos64 <manosov1984@gmail.com>
2022-08-06 16:45:50 +0300

    fix: del comment

--- online-store/src/modules/controller/volumeSlider.ts
+++ online-store/src/modules/controller/volumeSlider.ts
@@ -1,4 +1,4 @@
-import * as noUiSlider from 'noUiSlider';
+import * as noUiSlider from 'nouislider';
 import 'nouislider/dist/nouislider.css';
 import './sliders.scss';
 import { data } from '../main/main';
@@ -56,69 +56,3 @@ export function createVolumeSlider() {
     filterGoods();
   });
 }
-
-// export interface Slider {
-//   (sliderName: noUiSlider.target | null,
-//   sliderClass: string,
-//   minRange: HTMLInputElement | null,
-//   maxRange: HTMLInputElement | null): void;
-// }
-
-// // eslint-disable-next-line max-len, max-len, max-len
-// export function createSlider<T> (sliderName: T, sliderClass: string,
-// minRange: HTMLInputElement | null, maxRange: HTMLInputElement | null) {
-//   const sliderName: noUiSlider.target | null = document.getElementById(sliderClass);
-
-//   if (!sliderName || !minRange || !maxRange) return;
-
-//   const arrVolume: number[] = [];
-//   data.forEach(item => arrVolume.push(+item.volume));
-//   arrVolume.sort((a, b) => a - b);
-
-//   const inputs = [minRange, maxRange];
-
-//   if (sliderName) {
-//     noUiSlider.create(sliderName, {
-//       start: [arrVolume[0], arrVolume[arrVolume.length - 1]],
-//       tooltips: [true, true],
-//       connect: true,
-//       range: {
-//         min: arrVolume[0],
-//         max: arrVolume[arrVolume.length - 1]
-//       },
-//       step: 1,
-//       format: {
-//         to: (value: number) => Math.round(value),
-//         from: (value: string) => Number((value))
-//       }
-//     });
-//   }
-
-//   sliderName.noUiSlider?.on('update', (values: (string | number)[], handle: number): void => {
-//     if (values) {
-//       inputs[handle].value = String(values[handle]);
-//     }
-//   });
-
-//   sliderName.noUiSlider?.on('slide', filterGoods);
-
-//   minRange.addEventListener('change', () => {
-//     sliderName.noUiSlider?.set([minRange.value, 'null']);
-//   });
-
-//   maxRange.addEventListener('change', () => {
-//     sliderName.noUiSlider?.set(['null', maxRange.value]);
-//   });
-
-//   resetFiltersBtn?.addEventListener('click', () => {
-//     sliderName.noUiSlider?.set([arrVolume[0], arrVolume[arrVolume.length - 1]]);
-//     filterGoods();
-//   });
-// };
-
-// const volumeSlider = createSlider(
-//   volumeSlider,
-//   'volume-slider',
-//   minVolume,
-//   maxVolume
-// );
